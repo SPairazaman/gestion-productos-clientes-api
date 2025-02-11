dc31f368e2e22fdb385ada76ff13cddd71b09394
Santos Pairazaman <spairazaman@hotmail.com>
2025-02-11 17:47:40 -0500

    Creando Productos + ProyectoFinalDB

---
+++ Domain/Entities/Producto.cs
@@ -0,0 +1,24 @@
+﻿using Domain.Contract;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Domain.Entities
+{
+    public class Producto:IAuditableEntity
+    {
+        public int ProductoID { get; set; }
+        [Required]
+        [StringLength(255)]
+        public string Nombre { get; set; }
+        [Required]
+        public decimal Precio { get; set; }
+        [Required]
+        public int Stock { get; set; }
+        public DateTime FechaCreacion { get; set; }
+        public bool Activo { get; set; }
+    }
+}
---
+++ Infraestructure/ProyectoFinalContext.cs
@@ -0,0 +1,18 @@
+﻿using Domain.Entities;
+using Microsoft.EntityFrameworkCore;
+
+namespace Infraestructure
+{
+    public class ProyectoFinalContext : DbContext
+    {
+        public DbSet<Producto> Productos { get; set; }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseSqlServer("Server=L-Lenovo1-SP\\SqlExpress2022;" +
+                                                            "Database=ProyectoFinalDB;User Id=UserSQL;Password=123456;" +
+                                                            "Trust Server Certificate=True");
+        }
+
+    }
+}
