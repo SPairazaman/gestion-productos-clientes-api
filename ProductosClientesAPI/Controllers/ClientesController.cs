0fb41df53850aa115ac53a16b7408916efa94e1c
Santos Pairazaman <spairazaman@hotmail.com>
2025-02-11 22:52:14 -0500

    Api Clientes

---
+++ ProductosClientesAPI/Controllers/ClientesController.cs
@@ -0,0 +1,33 @@
+﻿using Domain.Entities;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Services;
+
+namespace ProductosClientesAPI.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ClientesController : ControllerBase
+    {
+
+        [HttpGet]
+        public List<Cliente> GetAsync(string? filtro)
+        {
+            ClienteService clienteService = new ClienteService();
+            return clienteService.Get(filtro);
+        }
+
+        [HttpPost]
+        public void CreateAsync(Cliente cliente) {
+            ClienteService clienteService = new ClienteService();
+            clienteService.Create(cliente);
+        }
+
+        [HttpDelete]
+        public void DeleteAsync(int id) {
+            ClienteService clienteService = new ClienteService();
+            clienteService.Delete(id);
+        }
+
+    }
+}
---
+++ Services/ClienteService.cs
@@ -0,0 +1,54 @@
+﻿using Domain.Entities;
+using Infraestructure;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.IdentityModel.Tokens;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Services
+{
+    public class ClienteService
+    {
+        public List<Cliente> Get(string filtro)
+        {
+            using (var _context = new ProyectoFinalContext())
+            {
+                var query = _context.Clientes.Where(c => c.Activo == true);
+
+                if (!filtro.IsNullOrEmpty())
+                {
+                    query = query.Where(c => c.Nombre.Contains(filtro));
+                }
+                return query.ToList();
+            }
+        }
+
+        public void Create(Cliente cliente)
+        {
+            using (var _context = new ProyectoFinalContext())
+            {
+                cliente.Activo = true;
+                cliente.FechaCreacion =DateTime.Now;
+                _context.Clientes.Add(cliente);
+                _context.SaveChanges();
+            }
+        }
+
+        public void Delete(int Id)
+        {
+            using (var _context = new ProyectoFinalContext())
+            {
+                Cliente cliente = _context.Clientes.Find(Id);
+                if (cliente != null) { 
+                    _context.Entry(cliente).State=EntityState.Modified;
+                    cliente.Activo=false;
+                    _context.SaveChanges();
+                }
+            }
+        }
+
+    }
+}
